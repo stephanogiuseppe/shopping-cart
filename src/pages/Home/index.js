51a021e5d79caf0eb233dd709ee7df9eb75e45b1
stephanogiuseppe <stephano.grp@gmail.com>
2020-01-05 23:11:13 -0300

    add polished lib and add home style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,84 @@
 import React from 'react'
+import { MdAddShoppingCart } from 'react-icons/md'
+
+import { Products } from './styles'
 
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <Products>
+      <li>
+        <img src="https://picsum.photos/200/300" alt="random image" />
+        <strong>Random image</strong>
+        <span>R$ 219,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://picsum.photos/200/300" alt="random image" />
+        <strong>Random image</strong>
+        <span>R$ 219,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://picsum.photos/200/300" alt="random image" />
+        <strong>Random image</strong>
+        <span>R$ 219,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://picsum.photos/200/300" alt="random image" />
+        <strong>Random image</strong>
+        <span>R$ 219,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://picsum.photos/200/300" alt="random image" />
+        <strong>Random image</strong>
+        <span>R$ 219,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://picsum.photos/200/300" alt="random image" />
+        <strong>Random image</strong>
+        <span>R$ 219,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} /> 3
+          </div>
+          <span>Add to cart</span>
+        </button>
+      </li>
+    </Products>
+  )
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components'
+import { darken } from 'polished'
+
+export const Products = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  list-style: none;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
+
+    img {
+      align-self: center;
+      max-width: 250px;
+    }
+
+    > strong {
+      font-size: 16px;
+      line-height: 20px;
+      color: #333;
+      margin-top: 5px;
+    }
+
+    > span {
+      font-size: 21px;
+      font-weight: bold;
+      margin: 5px 0 20px;
+    }
+
+    button {
+      background: #9e4040;
+      border: 0;
+      border-radius: 4px;
+      overflow: hidden;
+      margin-top: auto;
+      display: flex;
+      align-items: center;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#9e4040')};
+      }
+
+      * {
+        color: #fff;
+      }
+
+      div {
+        display: flex;
+        align-items: center;
+        padding: 12px;
+        background: #ffffff11;
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+
+      span {
+        flex: 1;
+        text-align: center;
+        font-weight: bold;
+      }
+    }
+  }
+`
