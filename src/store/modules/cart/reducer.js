8c91f79dea2c23244f0ba0c5731de85f64da1a3e
stephanogiuseppe <stephano.grp@gmail.com>
2020-01-09 23:35:45 -0300

    change add responsability: from reducer to sagas

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,23 +1,8 @@
 export default function cart(olderProducts = [], action) {
   switch (action.type) {
-    case 'ADD_PRODUCT_TO_CART_SUCCESS': {
-      const productIndex = olderProducts.findIndex(
-        item => item.id === action.product.id
-      )
+    case 'ADD_PRODUCT_TO_CART_SUCCESS':
+      return [action.product]
 
-      if (productIndex >= 0) {
-        olderProducts[productIndex].amount++
-        return [...olderProducts]
-      }
-
-      return [
-        ...olderProducts,
-        {
-          ...action.product,
-          amount: 1
-        }
-      ]
-    }
     case 'REMOVE_PRODUCT_FROM_CART': {
       const productIndex = olderProducts.findIndex(
         item => item.id === action.id
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,12 +1,26 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects'
+import { call, select, put, all, takeLatest } from 'redux-saga/effects'
 
 import api from './../../../services/api'
-import { addProductToCartSuccess } from './actions'
+import { formatPriceToBRMask } from './../../../util/format'
+import { addProductToCartSuccess, updateAmountProductFromCart } from './actions'
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`)
+  const productExists = yield select(state => state.cart.find(p => p.id === id))
 
-  yield put(addProductToCartSuccess(response.data))
+  if (productExists) {
+    const amount = ++productExists.amount
+    yield put(updateAmountProductFromCart(id, amount))
+  } else {
+    const response = yield call(api.get, `/products/${id}`)
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPriceToBRMask(response.data.price)
+    }
+
+    yield put(addProductToCartSuccess(data))
+  }
 }
 
 export default all([takeLatest('ADD_PRODUCT_TO_CART_REQUEST', addToCart)])
